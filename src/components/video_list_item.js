2cd415cb92ad48ec0c618b78437244cd451fffd7
suchetamandal <sucheta.gcetts@gmail.com>
2018-01-01 21:34:06 -0800

    app for searching video through youtube API in a youtube like view structure.

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = (props) =>{
+    const video = props.video;
+    const onVideoSelect = props.onVideoSelect;
+    const imageURL = video.snippet.thumbnails.default.url;
+    return(
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list">
+                <div className="media-left">
+                    <img className="media-object" src={imageURL} />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>    
+        </li>    
+    );
+};
+
+export default VideoListItem;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyD-dT1V-gGvvfHmVx65tTNj9vbheKwBIGo';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = { 
+            videos : [] ,
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards')
+    }
+
+    videoSearch(term){
+        console.log('Searching Term '+term);
+        YTSearch({key:API_KEY, term:term}, (videos)=>{
+              this.setState({
+                    videos:videos,
+                    selectedVideo:videos[0]
+                });
+            });
+    }
+
+    render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+        return(
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect = {(selectedVideo) => this.setState({selectedVideo:selectedVideo})}
+                    videos = {this.state.videos}
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
